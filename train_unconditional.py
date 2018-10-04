d5b285bab004937c62d3ce67580eb476ebf2c443
Yongbin Sun <yb_sun@mit.edu>
2018-10-04 14:06:43 -0400

    update train_unconditional.py

--- train_unconditional.py
+++ train_unconditional.py
@@ -23,7 +23,6 @@ parser.add_argument('--num_output', type=int, default=200, help='The number of d
 parser.add_argument('--batch_size', type=int, default=15, help='Batch Size during training [default: 32]')
 parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
-parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
 parser.add_argument('--decay_step', type=int, default=200000, help='Decay step for lr decay [default: 200000]')
 parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.8]')
 FLAGS = parser.parse_args()
